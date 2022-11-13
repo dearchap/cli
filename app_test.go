30261ac33d91906c79c2eb41511c604758bdbccb
Naveen Gogineni <dear.chap@gmail.com>
2022-11-12 21:14:57 -0500

    Add FlagConfig

--- app_test.go
+++ app_test.go
@@ -2654,6 +2654,7 @@ func TestFlagAction(t *testing.T) {
 	stringFlag := &StringFlag{
 		Name: "f_string",
 		Action: func(c *Context, v string) error {
+			t.Log("in adction")
 			if v == "" {
 				return fmt.Errorf("empty string")
 			}
--- flag_uint64.go
+++ flag_uint64.go
@@ -6,27 +6,33 @@ import (
 )
 
 // -- uint64 Value
-type uint64Value uint64
+type uint64Value struct {
+	val  *uint64
+	base int
+}
 
-func (i uint64Value) Create(val uint64, p *uint64) flag.Value {
+func (i uint64Value) Create(val uint64, p *uint64, c FlagConfig) flag.Value {
 	*p = val
-	return (*uint64Value)(p)
+	return &uint64Value{
+		val:  p,
+		base: c.IntBase(),
+	}
 }
 
 func (i *uint64Value) Set(s string) error {
-	v, err := strconv.ParseUint(s, 0, 64)
+	v, err := strconv.ParseUint(s, i.base, 64)
 	if err != nil {
 		return err
 	}
-	*i = uint64Value(v)
+	*i.val = v
 	return err
 }
 
-func (i *uint64Value) Get() any { return uint64(*i) }
+func (i *uint64Value) Get() any { return uint64(*i.val) }
 
-func (i *uint64Value) String() string { return strconv.FormatUint(uint64(*i), 10) }
+func (i *uint64Value) String() string { return strconv.FormatUint(uint64(*i.val), 10) }
 
-type Uint64Flag = flagImpl[uint64, uint64Value]
+type Uint64Flag = FlagBase[uint64, uint64Value]
 
 // Int64 looks up the value of a local Int64Flag, returns
 // 0 if not found
